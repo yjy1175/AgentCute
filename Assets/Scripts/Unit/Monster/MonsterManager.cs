05bbccb118dde22b38f201a8f234a5c47c8475e4
jh20s <jh20s@naver.com>
2022-04-05 17:32:35 +0900

    feat(Monster DataSet): set Monster Data set
    csv로부터 MonsterManager에 monster data read

--- Assets/Scripts/Unit/Monster/MonsterManager.cs
+++ Assets/Scripts/Unit/Monster/MonsterManager.cs
@@ -4,6 +4,36 @@ using UnityEngine;
 
 public class MonsterManager : SingleToneMaker<MonsterManager>
 {
+ 
+    //TO-DO : 어떤식으로 관리할지는 고민 class로 빼야 다른쪽에서도 객체 생성하기 용이할거로 보임 아님 
+    public struct MonsterData
+    {
+        public int id;
+        public string monsterSpawnMap;
+        public string monsterInName;
+        public int monsterSize;
+        public string monsterName;
+        public string monsterGrade;
+        public int monsterPhysicalDefense;
+        public int monsterMagicDefense;
+        public int monsterSpeed;
+        public int closeAttackPower;
+        public int closeAttackRange;
+        public int standoffAttackPower;
+        public int standoffAttackRange;
+        //public int MonsterAI;
+        public int monsterExp;
+
+        //public int MonsterDrop1;
+        //public int MonsterDrop2;
+        //public int MonsterDrop3;
+    }
+    public List<MonsterData> dataSet;
+    private void Awake()
+    {
+        InitAllSpawnData();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +43,7 @@ public class MonsterManager : SingleToneMaker<MonsterManager>
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
     /*
@@ -47,4 +77,44 @@ public class MonsterManager : SingleToneMaker<MonsterManager>
 
         return temp;
     }
+
+    /*
+     * CSVFile\\MonsterData의 data를 읽어서 dataSet을 만듬
+     */
+    private void InitAllSpawnData()
+    {
+        dataSet = new List<MonsterData>();
+        List<Dictionary<string, object>> spawnData = CSVReader.Read("CSVFile\\MonsterData");
+        for (int idx = 0; idx < spawnData.Count; idx++)
+        {
+
+            MonsterData md = new MonsterData();
+            md.id = (int)spawnData[idx]["ID"];
+            md.monsterSpawnMap = spawnData[idx]["MonsterSpawnMap"].ToString();
+            md.monsterInName = spawnData[idx]["MonsterInName"].ToString();
+            md.monsterSize = (int)spawnData[idx]["MonsterSize"];
+            md.monsterName = spawnData[idx]["MonsterName"].ToString();
+            md.monsterGrade = spawnData[idx]["MonsterGrade"].ToString();
+            md.monsterPhysicalDefense = (int)spawnData[idx]["MonsterPhysicalDefense"];
+            md.monsterMagicDefense = (int)spawnData[idx]["MonsterMagicDefense"];
+            md.monsterSpeed = (int)spawnData[idx]["MonsterSpeed"];
+            md.closeAttackPower = (int)spawnData[idx]["CloseAttackPower"];
+            md.closeAttackRange = (int)spawnData[idx]["CloseAttackRange"];
+            md.standoffAttackPower = (int)spawnData[idx]["StandoffAttackPower"];
+            md.standoffAttackRange = (int)spawnData[idx]["StandoffAttackRange"];
+            md.monsterExp = (int)spawnData[idx]["MonsterExp"];
+            //ds.CloseAttackAnimation = (int)spawnData[idx]["CloseAttackAnimation"];
+            //ds.StandoffAttackAnimation = (int)spawnData[idx]["StandoffAttackAnimation"];
+            //ds.HitAnimation = (int)spawnData[idx]["HitAnimation"];
+            //ds.MonsterAI = (int)spawnData[idx]["MonsterAI"];
+            //ds.spawnStartTime_2 = (int)spawnData[idx]["ChaseAnimation"];
+            //ds.spawnStartTime_2 = (int)spawnData[idx]["DeathAnimation"];
+            //ds.MonsterDrop1 = (int)spawnData[idx]["MonsterDrop1"];
+            //ds.MonsterDrop2 = (int)spawnData[idx]["MonsterDrop2"];
+            //ds.MonsterDrop3 = (int)spawnData[idx]["MonsterDrop3"];
+            dataSet.Add(md);
+        }
+    }
+
+
 }
