bc5069c54944df5c22897bb7d16d3c78ba298d76
jh20s <jh20s@naver.com>
2022-05-05 06:19:35 +0900

    feat(move) : Monster AI Move Algorithm
    1. Monster AI알고리즘 적용
    Player transform 기준에서 RayCast를 활용하여
    특정 크기만큼 타일맵에 대한 정보를 얻어온다.
    얻어온 타일맵에 대해 BFS를 돌려 Player기준으로
    이동가능한 타일맵에 대해 최단거리를 구한다.
    2. Monster는 Player가 구해놓은 BFS데이터를 확인하여
    해당 데이터의 8방향을 확인하여 가장 값이 낮은곳으로 이동한다.

--- Assets/Scripts/Unit/Monster/MonsterMove/MonsterMove.cs
+++ Assets/Scripts/Unit/Monster/MonsterMove/MonsterMove.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class MonsterMove : IMove
 {
+    private bool DEBUG = true;
+    private Vector3[] mVectorDir = new Vector3[8] { Vector3.up, Vector3.down, Vector3.left, Vector3.right,
+            Vector3.up+ Vector3.left, Vector3.up+ Vector3.right, Vector3.down+ Vector3.left, Vector3.down+ Vector3.right};
+
     [SerializeField]
     private GameObject mTarget;
     // Start is called before the first frame update
@@ -18,77 +22,26 @@ public class MonsterMove : IMove
     void Update()
     {
         /*
-         * TO-DO : 몬스터 무브가 다양해지면 MonsterMoveStrategy 클래스를 만들어
-         *         Stragegy패턴을 적용해서 추가할것
-         *         우선 Player에게 다가가는 형태만으로 구성.
+         * TO-DO Wave별 길찾기 알고리즘 패턴 적용 필요
         */
-        MovingPattern1();
-        //MovingPattern2();
-        
-    }
-    IEnumerator SetDir()
-    {
-        if(mDir != Vector3.zero)
-        {
-            yield return new WaitForSeconds(1f);
-            mDir = Vector3.zero;
-        }
-    }
-    //벽 무시없이 distance로만 이동하는 패턴
-    void MovingPattern1()
-    {
-        mTarget = GameObject.Find("UnitRoot");
-        if (mTarget != null)
-        {
-            
-           
-
-            mDir = mTarget.transform.position - transform.position;
-            mDir.Normalize();
-        }
-    }
-    //몬스터 기준 8방향에대해서 막힌 길일 경우는 우회하는 알고리즘
-    public void MovingPattern2()
-    {
-        if (mDir == Vector3.zero)
-        {
-            bool[] arr = new bool[8];
-            float minDistance = float.MaxValue;
-            int guessDir = 0;
-
-            for (int i = 0; i < 8; i++)
-            {
-                if (MoveableWithRay(i))
-                {
-                    Vector3 dir = new Vector3(mRayDr[i], mRayDc[i], 0).normalized + gameObject.transform.position;
-                    float distance = Vector3.Distance(dir, PlayerManager.Instance.Player.transform.position);
-                    if (minDistance > distance)
-                    {
-                        guessDir = i;
-                        minDistance = distance;
-                    }
-                }
-            }
-            mDir = new Vector3(mRayDr[guessDir], mRayDc[guessDir], 0);
-            mDir.Normalize();
-            StartCoroutine(SetDir());
-            Debug.DrawRay(transform.position, mDir, Color.red);
-        }
+        //MovingPattern1();
+        MovingPattern2();
+        //MovingPattern3();
     }
 
     private void FixedUpdate()
     {
         // 모든 물리연산은 FixedUpdate 에서
-        if(mTarget != null && mMoveable) { 
+        if (mMoveable) {
             transform.Translate(mDir * mSpeed * Time.deltaTime);
             int size = gameObject.GetComponent<IStatus>().Size;
-            if(gameObject.transform.position.x < PlayerManager.Instance.Player.transform.position.x)
+            if (gameObject.transform.position.x < PlayerManager.Instance.Player.transform.position.x)
             {
                 gameObject.transform.localScale = new Vector3(size, size, size);
             }
             else
             {
-                gameObject.transform.localScale = new Vector3(-1*size, size, size);
+                gameObject.transform.localScale = new Vector3(-1 * size, size, size);
             }
         }
     }
@@ -96,19 +49,45 @@ public class MonsterMove : IMove
     {
         base.StopStiffTime(_time);
     }
-    // 좌하, 좌, 좌상
-    private int[] mRayDr = new int[8] { -1, 0, 1, -1, 1, -1, 0, 1 };
-    private int[] mRayDc = new int[8] { -1, -1, -1, 0, 0, 1, 1, 1 };
 
-    public bool MoveableWithRay(int i )
+    //플레이어와 몬스터 객체의 단위 벡터방향으로 움직이는 길찾기 알고리즘
+    void MovingPattern1()
     {
-        Vector3 dir = new Vector3(mRayDr[i], mRayDc[i], 0).normalized;
-        RaycastHit2D ray = Physics2D.Raycast(transform.position, dir, 1f, LayerMask.GetMask("Tilemap"));
-        if (ray.collider != null)
+        mDir = PlayerManager.Instance.Player.transform.position - transform.position;
+        mDir.Normalize();
+    }
+
+
+    //Player가 업데이트한 BFS노드를 확인하여 가장 가중치가 낮은곳으로 이동
+    public void MovingPattern2()
+    {
+        mTarget = PlayerManager.Instance.Player;
+        int value = 99999;
+        Vector3 dir = Vector3.right;
+
+        //8방향에 대해 PlayerBfs에서 가장 가중치가 낮은 곳으로 이동
+        for (int i = 0; i < 8; i++)
         {
-            return false;
-        } 
-        else
-            return true;
+            Vector3 nextPosition = Vector3Int.FloorToInt(transform.position) + mVectorDir[i];
+            nextPosition.x += 0.5f;
+            nextPosition.y += 0.5f;
+            if (!PlayerManager.Instance.Player.GetComponent<PlayerMove>().BfsMap.ContainsKey(nextPosition))
+                continue;
+            if(PlayerManager.Instance.Player.GetComponent<PlayerMove>().BfsMap[nextPosition].isMove
+                    && PlayerManager.Instance.Player.GetComponent<PlayerMove>().BfsMap[nextPosition].value< value)
+            {
+                value = PlayerManager.Instance.Player.GetComponent<PlayerMove>().BfsMap[nextPosition].value;
+                dir = nextPosition;
+
+            }
+        }
+        mDir = dir - transform.position;
+        mDir.Normalize();
+
+        //Player의 BFS탐색 범위 밖이면  Moving Pattern1을 적용
+        if (value == 99999) {
+            MovingPattern1();
+        }
+
     }
 }
--- Assets/Scripts/Unit/Player/PlayerMove.cs
+++ Assets/Scripts/Unit/Player/PlayerMove.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class PlayerMove : IMove
 {
+    private bool DEBUG = false;
+
     private Animator mAnim;
     public Animator MAnim
     {
         get { return mAnim; }
         set { mAnim = value; }
     }
+
     [SerializeField]
     private VertualJoyStick mJoyStick;
+
+    public struct Node
+    {
+        public bool isMove;
+        public int value;
+    }
+
+    private Dictionary<Vector3, Node> mBfsMap = new Dictionary<Vector3, Node>();
+    public Dictionary<Vector3, Node> BfsMap
+    {
+        get { return mBfsMap; }
+    }
+
+    private Vector3[] mVectorDir = new Vector3[8] { Vector3.up, Vector3.down, Vector3.left, Vector3.right,
+            Vector3.up+ Vector3.left, Vector3.up+ Vector3.right, Vector3.down+ Vector3.left, Vector3.down+ Vector3.right};
+    private float raydist = 0.49f;
+    private Vector3 mRayPosition;
+
+
     void Awake()
     {
         mAnim = transform.GetChild(1).GetComponent<Animator>();
@@ -22,6 +44,8 @@ public class PlayerMove : IMove
     void Update()
     {
         UpdateMove();
+
+        MovingStartegy();
     }
 
 
@@ -82,4 +106,121 @@ public class PlayerMove : IMove
         yield return new WaitForSeconds(_time);
         mAnim.SetFloat("RunState", 0f);
     }
+
+    private void MovingStartegy()
+    {
+        mBfsMap.Clear();
+        mBfsMap = new Dictionary<Vector3, Node>();
+        MakeMap();
+        GetBFS();
+    }
+
+    //MakeMap에 의해 만들어진 맵에서 BFS를 돌려 유저기준 최단거리를 구해놓는다.
+    //몬스터가 구해진 BFS값을 확인하여 8방향에서 유저에게 가장 가까운 위치를 체크할수 있음
+    private void GetBFS()
+    {
+        Dictionary<Vector3, bool> check = new Dictionary<Vector3, bool>();
+        Queue<Vector3> q = new Queue<Vector3>();
+        Vector3 startPos = Vector3Int.FloorToInt(transform.position);
+        startPos.x += 0.5f;
+        startPos.y += 0.5f;
+        Node startnode = new Node();
+        startnode.isMove = false;
+        startnode.value = 0;
+
+        q.Enqueue(startPos);
+        mBfsMap[startPos] = startnode;
+        while (q.Count != 0)
+        {
+            Vector3 nowPosition = q.Dequeue();
+            for (int i = 0; i < 4; i++)
+            {
+                Vector3 nextPosition = nowPosition + mVectorDir[i];
+                if (mBfsMap.ContainsKey(nextPosition))
+                {
+                    if (mBfsMap[nextPosition].isMove && !check.ContainsKey(nextPosition))
+                    {
+                        check[nextPosition] = true;
+                        Node nextNode = mBfsMap[nextPosition];
+                        nextNode.value = mBfsMap[nowPosition].value + 1;
+                        mBfsMap[nextPosition] = nextNode;
+                        q.Enqueue(nextPosition);
+                    }
+                }
+            }
+        }
+    }
+
+    //RayCase를 쏘아 유저기준 Size크기의 Vector3를 key로 갖는 딕셔너리를 만든다.
+    private void MakeMap()
+    {
+        //TO-DO frame drop이 없는 선 안에서 모든 몬스터가 쫓아오도록 size 조정필요
+        int Size = 25;
+        int count = 0;
+        for (int i = 0; i < Size; i++)
+        {
+            for (int j = 0; j < Size; j++)
+            {
+                Vector3 targetPosition = Vector3Int.FloorToInt(transform.position);
+
+                Node node = new Node();
+                node.isMove = MoveableWithRay(Size / 2 - i, Size / 2 - j);
+                node.value = 99999;
+                mBfsMap[mRayPosition] = node;
+                if (DEBUG)
+                {
+                    if (!node.isMove)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        if (DEBUG)
+        {
+            Debug.Log("장애물 개수" + count);
+            Debug.Log("MakeMap 이후 mBfsMap의크기" + mBfsMap.Count);
+        }
+    }
+
+    //object위치에서 x,y거리만큼 떨어진곳에 장애물이 있는지 확인
+    //타일의 정중앙에서 rayCaset를 발사하여 장애물이 있는지 체크
+    //장애물이 없으면 true , 있으면 false를 리턴
+    private bool MoveableWithRay(int x, int y)
+    {
+        Vector3 targetPosition = transform.position;
+        targetPosition.x += x;
+        targetPosition.y += y;
+        float distance = Vector3.Distance(targetPosition, transform.position);
+
+
+        Vector3 dir = (targetPosition - transform.position).normalized * distance;
+        Vector2 rayTargetPosition = new Vector2(transform.position.x + dir.x, transform.position.y + dir.y);
+
+        //rayTargetPosition의 위치를 tile의 한가운데로 위치시켜줌
+        rayTargetPosition = Vector2Int.FloorToInt(rayTargetPosition);
+        rayTargetPosition.x += 0.5f;
+        rayTargetPosition.y += 0.5f;
+        mRayPosition = rayTargetPosition;
+
+        for (int i = 0; i < 4; i++)
+        {
+            RaycastHit2D ray = Physics2D.Raycast(rayTargetPosition, mVectorDir[i], raydist, LayerMask.GetMask("Tilemap"));
+            if (ray.collider != null)
+            {
+                if (DEBUG)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        Vector3 debugVector = mVectorDir[j] * raydist;
+                        Debug.DrawRay(new Vector2(rayTargetPosition.x, rayTargetPosition.y), debugVector, Color.red);
+                    }
+                }
+                return false;
+            }
+        }
+        return true;
+
+    }
+
 }
